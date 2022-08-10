9d03518307200ea5af1d9b834d6ed4a29050e3d3
João Paulo Mesquita <jp-mesquita@live.com>
2022-08-10 20:21:15 -0300

    adding react-forms, context and json-server

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,40 @@
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as Dialog from '@radix-ui/react-dialog'
-import * as RadioGroup from '@radix-ui/react-radio-group'
+import * as z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { useForm } from 'react-hook-form'
+
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome']),
+})
+
+
+type NewTransactionFromInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal(){
+
+  const {
+    register,
+    handleSubmit,
+    formState:{ isSubmitting }
+  } = useForm<NewTransactionFromInputs>({
+    resolver: zodResolver(newTransactionFormSchema)
+  })
+
+  async function handleCreateNewTransaction(data: NewTransactionFromInputs){
+
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+
+    console.log(data)
+  }
+
   return(
     <Dialog.Portal>
       <Overlay/>
@@ -15,10 +45,25 @@ export function NewTransactionModal(){
         <CloseButton>
           <X size={24}/>
         </CloseButton>
-        <form action="">
-          <input type="text" placeholder='Descrição' required/>
-          <input type="number" placeholder='Preço' required/>
-          <input type="text" placeholder='Categoria' required/>
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input 
+            type="text"   
+            placeholder='Descrição' 
+            required
+            {...register('description')}
+          />
+          <input 
+            type="number"   
+            placeholder='Preço' 
+            required
+            {...register('price', {valueAsNumber: true})}
+          />
+          <input 
+            type="text" 
+            placeholder='Categoria' 
+            required
+            {...register('category')}
+            />
 
           <TransactionType>
             <TransactionTypeButton variant='income' value='income'>
@@ -31,7 +76,7 @@ export function NewTransactionModal(){
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type='submit'>
+          <button type='submit' disabled={isSubmitting}>
             Cadastrar
           </button>
         </form>
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext, useTransactions } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionTable } from "./styles";
 
+
+
 export function Transactions(){
+
+  const { transactions } = useTransactions()
+
   return(
-    <div>
+    <div> 
       <Header />
       <Summary />
 
@@ -13,26 +21,22 @@ export function Transactions(){
         <SearchForm />
         <TransactionTable>
           <tbody>
-            <tr>
-              <td width="50%">Desevolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">
-                  R$ 12.000,00  
-                </PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighLight variant="outcome">
-                  - R$ 59,00
-                </PriceHighLight>
-              </td>
-              <td>Aliemtação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map(transaction => {
+              return(
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type ==  "outcome" && "- "}
+                     {priceFormatter.format(transaction.price)}  
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                </tr>
+              )
+            })}
+
           </tbody>
         </TransactionTable>
       </TransactionsContainer>
